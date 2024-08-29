926329362fc1cbe73a4096f7acdba4b275e4f76d
Kenny <101839505+khui0@users.noreply.github.com>
2024-08-29 16:12:14 -0400

    feat: more

--- src/routes/Calendar.svelte
+++ src/routes/Calendar.svelte
@@ -4,6 +4,13 @@
 
   import PhClock from "~icons/ph/clock";
 
+  function timeToS(time: string) {
+    const parts = time.split(":");
+    const hours = parseInt(parts[0]) * 3600;
+    const minutes = parseInt(parts[1]) * 60;
+    return hours + minutes;
+  }
+
   let showWeekend: boolean = false;
 
   interface Item {
@@ -18,18 +25,27 @@
 
   let data: Item[][] = [];
 
+  let time: string;
+  let progress: number = -1;
+
   onMount(() => {
     pb.collection("schedules")
       .getFullList()
       .then((res) => (data = res[0].schedule));
-  });
 
-  function timeToS(time: string) {
-    const parts = time.split(":");
-    const hours = parseInt(parts[0]) * 3600;
-    const minutes = parseInt(parts[1]) * 60;
-    return hours + minutes;
-  }
+    setInterval(() => {
+      const now = new Date();
+      const tzo = now.getTimezoneOffset() * 60000;
+      const hour = ((Date.now() % 8.64e7) - tzo) / 3.6e6;
+      progress = (hour - 8) / 12;
+      time = now
+        .toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "2-digit",
+        })
+        .replace(/AM|PM/, "");
+    }, 100);
+  });
 </script>
 
 <div class="flex flex-col">
@@ -44,32 +60,32 @@
     </div>
   </div>
   <div
-    class="z-10 sticky top-0 mx-4 py-2 bg-base-100 text-center text-sm border-b grid {showWeekend
+    class="z-20 sticky top-0 mx-4 py-2 bg-base-100 text-sm border-b grid {showWeekend
       ? 'grid-cols-[3rem_repeat(7,1fr)]'
       : 'grid-cols-[3rem_repeat(5,1fr)]'}"
   >
-    <p class="flex justify-center"><PhClock></PhClock></p>
+    <p class="flex items-center justify-center"><PhClock></PhClock></p>
     {#each Array(showWeekend ? 7 : 5) as _, day}
       {@const days = ["M", "T", "W", "T", "F", "S", "S"]}
-      <p class:font-bold={new Date().getDay() === (day + 1) % 7}>{days[day]}</p>
+      {@const today = new Date().getDay() === (day + 1) % 7}
+      <button class="btn font-normal min-h-0 h-6" class:btn-ghost={!today}>{days[day]}</button>
     {/each}
   </div>
   {#each Array(13) as _, i}
-    <!-- Row -->
     <div
-      class="mx-4 text-center h-16 grid {showWeekend
+      class="relative mx-4 text-center h-16 grid {showWeekend
         ? 'grid-cols-[3rem_repeat(7,1fr)]'
         : 'grid-cols-[3rem_repeat(5,1fr)]'}"
       class:border-b={i !== 12}
     >
-      <p class="text-sm text-base-content/50 self-center">{i + 8}:00</p>
+      <p class="text-sm text-base-content/50 self-center">{i !== 4 ? (i + 8) % 12 : 12}:00</p>
       {#each Array(showWeekend ? 7 : 5) as _, j}
         {@const event = data[j]?.find((item) => parseInt(item.from.split(":")[0]) === i + 8)}
         {#if event}
           {@const length = (timeToS(event.to) - timeToS(event.from)) / 3600}
           <div class="relative">
-            <div
-              class="absolute top-0 w-full bg-accent text-accent-content rounded-md text-xs h-4"
+            <button
+              class="absolute z-10 top-0 w-full bg-accent text-accent-content rounded-md text-xs h-4 flex flex-col items-center justify-center"
               style="height: {length * 4}rem; transform: translateY({(parseInt(
                 event.from.split(':')[1],
               ) /
@@ -79,12 +95,19 @@
               <p>{event.class} {event.number}</p>
               <p>{event.type}</p>
               <p>{event.from} - {event.to}</p>
-              <p>{event.location}</p>
-              <p>{event.room}</p>
-            </div>
+              <p>{event.location} {event.room}</p>
+            </button>
           </div>
         {:else}
           <div></div>
+        {/if}
+        {#if i === 0}
+          <div class="z-10 absolute w-full" style="transform: translateY({progress * 48}rem)">
+            <div class="px-4 w-full h-0.5 bg-accent"></div>
+            <p class="bg-accent text-accent-content rounded-badge text-sm w-12 -translate-y-1/2">
+              {time}
+            </p>
+          </div>
         {/if}
       {/each}
     </div>
