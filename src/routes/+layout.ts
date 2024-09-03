73e82c09928f6038e83e2f584a17a658316cf96f
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-03 13:39:52 -0400

    Revert "feat: vercel analytics"
    This reverts commit 1de4ec6431c88a9d5d1cd3871e23c1b0db55fd4f.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,3 @@
 import "../app.css";
 import "@fontsource-variable/inter";
 import "$lib/theme";
-
-import { dev } from "$app/environment";
-import { inject } from "@vercel/analytics";
-
-inject({ mode: dev ? "development" : "production" });
