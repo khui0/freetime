0917263d523fd730baee53b35a3c1f1d4bf06963
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-03 03:58:05 -0400

    feat: padding

--- src/routes/friends/Friend.svelte
+++ src/routes/friends/Friend.svelte
@@ -7,8 +7,8 @@
   export let action: string = "Remove";
 </script>
 
-<button class="flex gap-1 items-center justify-between p-2" on:click>
-  <p>{username}</p>
+<button class="flex gap-1 items-center justify-between py-2" on:click>
+  <p class="pl-1">{username}</p>
   <button
     class="btn btn-sm"
     on:click={() => {
