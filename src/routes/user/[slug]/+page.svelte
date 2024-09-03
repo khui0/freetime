baa561e9b7e2f6a65041e835be778325d59759df
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-03 15:25:39 -0400

    feat: add modal to friend view

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -1,18 +1,50 @@
-<script>
+<script lang="ts">
   /** @type {import('./$types').PageData} */
   export let data;
 
   import { title } from "$lib/store";
   $title = data.username;
 
+  import { locations, types } from "$lib/sbu";
+  import { timeTo12Hour, eventDuration } from "$lib/time";
+
   import Calendar from "$lib/components/Calendar.svelte";
+  import Modal from "$lib/components/Modal.svelte";
+
+  let modal: Modal;
+  let selected: CalendarEvent;
 </script>
 
 {#if data?.schedule?.length > 0}
   <h2 class="font-bold text-2xl px-4 pt-2">{data.username}</h2>
-  <Calendar bind:data={data.schedule}></Calendar>
+  <Calendar
+    bind:data={data.schedule}
+    on:expand={(e) => {
+      selected = e.detail.selected;
+      modal.show();
+    }}
+  ></Calendar>
 {:else}
   <div class="grid h-full">
     <p class="text-base-content/50 text-xl place-self-center text-center">No schedule found</p>
   </div>
 {/if}
+
+<Modal title="{selected?.title} {selected?.number}" bind:this={modal}>
+  {#if selected}
+    <div class="flex gap-1 justify-between text-lg">
+      <h2>{types[selected?.type]}</h2>
+      <p>{timeTo12Hour(selected?.from, true)} - {timeTo12Hour(selected?.to, true)}</p>
+    </div>
+    <div class="flex gap-1 justify-between">
+      <p class="border border-base-content w-fit h-fit px-2 rounded-badge">{selected?.room}</p>
+      <p class="text-lg">{eventDuration(selected?.from, selected?.to)}</p>
+    </div>
+    <p>{locations[selected?.location].name}</p>
+    {#if locations[selected?.location].maps}
+      <a href={locations[selected?.location].maps} class="btn btn-sm self-start mb-1">
+        Open in Google Maps
+      </a>
+    {/if}
+  {/if}
+</Modal>
