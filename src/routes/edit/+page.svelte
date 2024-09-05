5be0db8b067f48ef78df44e54e0a29a6def539c1
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-05 14:57:19 -0400

    fix: padding

--- src/routes/edit/+page.svelte
+++ src/routes/edit/+page.svelte
@@ -102,7 +102,7 @@
   <div
     class="grid grid-cols-1 {events.length > 0
       ? 'md:grid-cols-2'
-      : ''} gap-4 overflow-auto py-2 flex-1"
+      : ''} gap-4 overflow-auto py-4 flex-1"
     use:scrollToBottom={events}
   >
     {#if events.length > 0}
