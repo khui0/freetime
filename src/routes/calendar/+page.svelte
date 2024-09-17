17ec44cb0a2048ed40cc4410a5359288f89789eb
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-17 12:49:28 -0400

    feat: update calendar

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -7,12 +7,15 @@
 
   import Calendar from "$lib/components/Calendar.svelte";
   import EventDetails from "$lib/components/EventDetails.svelte";
-
   import Onboarding from "$lib/components/Onboarding.svelte";
+
+  import PhPencilSimple from "~icons/ph/pencil-simple";
 
   let data: CalendarEvent[] = [];
 
   let details: EventDetails;
+
+  let date: string = "";
 
   onMount(async () => {
     if (!$currentUser) return;
@@ -22,20 +25,25 @@
     const list = await pb.collection("schedules").getFullList();
     const schedule = list.find((record) => record.user === $currentUser?.id);
     data = schedule?.schedule;
+
+    update();
+    setInterval(update, 1000);
+    function update() {
+      date = new Date().toLocaleDateString("en-US", {
+        weekday: "long",
+        day: "numeric",
+        month: "short",
+      });
+    }
   });
 </script>
 
 <div class="flex flex-col mt-2 pb-[49px]">
   <div class="px-4 pt-2 flex gap-2 items-center">
-    <button class="btn btn-sm">Today</button>
+    <h1 class="text-2xl font-bold">{date}</h1>
+    <!-- <button class="btn btn-sm">Today</button> -->
     <div class="ml-auto">
-      <a class="btn btn-sm" href="/edit">
-        {#if data.length > 0}
-          Edit schedule
-        {:else}
-          Add schedule
-        {/if}
-      </a>
+      <a class="btn btn-sm btn-square" href="/edit"><PhPencilSimple></PhPencilSimple></a>
     </div>
   </div>
   {#if data.length > 0}
