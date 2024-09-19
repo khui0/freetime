02be7e71fed8d9179c68cc089e076db543a9ab49
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-19 13:25:24 -0400

    feat: increase border radius

--- src/lib/components/CalendarItem.svelte
+++ src/lib/components/CalendarItem.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <button
-  class="absolute z-10 top-0 left-[1px] right-[1px] text-accent-content rounded-md text-xs h-4 flex flex-col items-center justify-center @container {!dim
+  class="absolute z-10 top-0 left-[1px] right-[1px] text-accent-content rounded-lg text-xs h-4 flex flex-col items-center justify-center @container {!dim
     ? 'bg-accent'
     : 'bg-accent/50'}"
   style="height: {length * 4}rem; transform: translateY({(parseInt(event.from.split(':')[1]) / 60) *
