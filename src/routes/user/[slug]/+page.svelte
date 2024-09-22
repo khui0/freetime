b7d366c4bb85d9b54913291b2c32511e5d0b0e2a
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-22 19:17:28 -0400

    feat: add today button to friend view

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -35,15 +35,26 @@
 
 {#if data?.schedule?.length > 0}
   <div class="px-4 pt-4 flex gap-2 items-center justify-between">
-    <button
-      class="btn btn-square btn-sm rounded-full"
-      on:click={() => {
-        history.back();
-      }}
-    >
-      <PhArrowLeft></PhArrowLeft>
-    </button>
-    <h2 class="font-bold text-2xl">{data.username}</h2>
+    <div class="flex gap-4 items-center flex-wrap">
+      <button
+        class="btn btn-square btn-sm rounded-full"
+        on:click={() => {
+          history.back();
+        }}
+      >
+        <PhArrowLeft></PhArrowLeft>
+      </button>
+      <h2 class="font-bold text-2xl">{data.username}</h2>
+    </div>
+    <div class="ml-auto flex gap-2 flex-wrap justify-end">
+      <button
+        class="btn btn-sm"
+        on:click={() => {
+          singleView = !singleView;
+          viewOffset = singleView ? (new Date().getDay() + 13) % 7 : 0;
+        }}>{!singleView ? "Today" : "Week"}</button
+      >
+    </div>
   </div>
   <div>
     <Calendar
