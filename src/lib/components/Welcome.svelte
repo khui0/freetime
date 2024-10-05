384b933faf0bab40a3a0754e4595aedab5996016
Kenny <101839505+khui0@users.noreply.github.com>
2024-10-05 11:34:46 -0400

    feat: add privacy policy to welcome

--- src/lib/components/Welcome.svelte
+++ src/lib/components/Welcome.svelte
@@ -10,8 +10,9 @@
     <a href="/sign-in" class="link text-sm">Continue with email</a>
   </div>
   <div class="absolute left-0 right-0 bottom-0 text-xs text-base-content/50 text-center p-4">
+    <a href="/privacy-policy" class="link">Privacy Policy</a>
     <p>
-      Having issues? Email <a class="font-bold" href="mailto:support@kennyhui.dev">
+      Having issues? Contact <a class="font-bold" href="mailto:support@kennyhui.dev">
         support@kennyhui.dev
       </a>
     </p>
