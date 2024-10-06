d7fdb5385662c695498746c03f72d9968559875d
Kenny <101839505+khui0@users.noreply.github.com>
2024-10-06 15:32:00 -0400

    feat: homepage

---
+++ src/lib/components/DemoFriend.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let name: string;
+  export let busy: boolean = false;
+</script>
+
+<div class="flex flex-col gap-4">
+  <div class="flex rounded-box border">
+    <div class="flex-1 flex items-center gap-2 py-2 px-4 rounded-l-box">
+      <div
+        class="w-3 h-3 rounded-full flex-none {busy
+          ? 'bg-warning'
+          : 'bg-success'} transition-colors"
+      ></div>
+      <p class="pl-1 py-2">{name}</p>
+      <div class="px-2 text-sm text-base-content/50 flex justify-between w-full flex-wrap gap-x-4">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,35 +19,112 @@
       }
     }
   });
+
+  import CalendarModalDetails from "$lib/components/CalendarModalDetails.svelte";
+  const sampleEvent: CalendarEvent = {
+    days: [false, true, false, true, false, false, false],
+    from: "15:30",
+    location: "eb",
+    number: "210",
+    room: "121",
+    title: "AMS",
+    to: "16:50",
+    type: "lecture",
+  };
+
+  import DemoFriend from "$lib/components/DemoFriend.svelte";
 </script>
 
-<main class="text-center p-10 mb-12">
+<main class="text-center p-10 w-[min(100%,800px)] mx-auto">
   <div class="mx-auto w-fit"><Logo></Logo></div>
   <p class="mt-5 text-center max-w-xlmx-auto leading-relaxed text-balance">
-    Social schedule viewer for Stony Brook University!
+    Social schedule viewer for Stony Brook University<sup>1</sup>
   </p>
   <div class="mt-5 flex gap-2 flex-wrap justify-center">
     <p class="border rounded-lg w-fit px-2 py-1">Navigate to your classes</p>
-    <p class="border rounded-lg w-fit px-2 py-1">see when they start (and end)</p>
-    <p class="border rounded-lg w-fit px-2 py-1">and share it with your friends!</p>
+    <p class="border rounded-lg w-fit px-2 py-1">See when they start (and end)</p>
+    <p class="border rounded-lg w-fit px-2 py-1">And share it with your friends!</p>
   </div>
+  <h2 class="mt-10 text-5xl font-bold tracking-tight">At a glance</h2>
+  <div class="mt-5 rounded-box border p-4 flex flex-col gap-2 w-full">
+    <CalendarModalDetails event={sampleEvent}></CalendarModalDetails>
+  </div>
+  <div class="mt-10 flex gap-5">
+    <enhanced:img
+      src="$lib/assets/promo/theme-split.png"
+      alt="Freetime promo image showcasing a dark and light image"
+      class="w-56"
+    />
+    <div class="flex flex-col items-start justify-center gap-5 text-start">
+      <h2 class="text-xl">View your schedule on a fluid calendar</h2>
+      <h2 class="text-xl">That works beautifully on mobile</h2>
+      <h2 class="text-xl">With multiple themes</h2>
+      <h2 class="text-xl">Including <b>Dark</b>...</h2>
+      <h2 class="text-xl"><i>Light</i>... and more!</h2>
+    </div>
+  </div>
+  <div class="-mt-20 flex gap-5 flex-row-reverse">
+    <enhanced:img
+      src="$lib/assets/promo/compare.png"
+      alt="Freetime promo image showcasing a dark and light image"
+      class="w-56"
+    />
+    <div class="flex flex-col items-end justify-center gap-5 text-end">
+      <h2 class="text-xl">Share your schedule with your friends</h2>
+      <h2 class="text-xl">See your schedules side by side</h2>
+      <h2 class="text-xl">And see when you both have <i>Freetime!</i></h2>
+    </div>
+  </div>
+  <div class="mt-5">
+    <h2 class="text-4xl font-bold">Who's free?</h2>
+    <div class="mt-4">
+      <DemoFriend name="Wolfie">
+        <p>SBU 101 starts in 0:30</p>
+        <p>1 class left</p>
+      </DemoFriend>
+    </div>
+    <div class="mt-4 opacity-50">
+      <DemoFriend name="Kenny" busy>
+        <p>WRT 102 ends in 1:45</p>
+        <p>2 classes left</p>
+      </DemoFriend>
+    </div>
+    <div class="mt-4 opacity-25">
+      <DemoFriend name="John">
+        <p>PHY 133 starts in 3:00</p>
+        <p>0 classes left</p>
+      </DemoFriend>
+    </div>
+  </div>
+  <div class="mt-5">
+    <h2 class="text-2xl font-bold">Other features</h2>
+    <ul class="mt-4 space-y-3">
+      <li>Import your schedule from SOLAR</li>
+      <li>Navigate to your classes with Google Maps™</li>
+    </ul>
+  </div>
+  <h2 class="mt-10 text-4xl font-bold">Get started</h2>
   <div class="mt-5">
     <GoogleAuth></GoogleAuth>
   </div>
   <div class="mt-5">
     <a href="/legacy-sign-in" class="link text-sm">Continue with email</a>
   </div>
+  <p class="mt-5 text-sm max-w-md mx-auto text-balance">
+    Your Google™ account is only used to sign you in with your email. No other data is collected.
+  </p>
+  <div class="mt-20 text-xs text-base-content/50 text-center">
+    <p>Google Maps™ is a trademark of Google LLC.</p>
+    <p><sup>1</sup>Not affiliated with Stony Brook University.</p>
+    <p>
+      Having issues? Contact <a class="link" href="mailto:support@kennyhui.dev">
+        support@kennyhui.dev
+      </a>
+    </p>
+    <p>
+      Copyright &copy; 2024 Kenny Hui. All rights reserved. <a href="/privacy-policy" class="link"
+        >Privacy Policy</a
+      >
+    </p>
+  </div>
 </main>
-<div class="fixed bottom-0 w-full text-xs text-base-content/50 text-center p-5">
-  <p>Not affiliated with Stony Brook University.</p>
-  <p>
-    Having issues? Contact <a class="link" href="mailto:support@kennyhui.dev">
-      support@kennyhui.dev
-    </a>
-  </p>
-  <p>
-    Copyright &copy; 2024 Kenny Hui. All rights reserved. <a href="/privacy-policy" class="link"
-      >Privacy Policy</a
-    >
-  </p>
-</div>
