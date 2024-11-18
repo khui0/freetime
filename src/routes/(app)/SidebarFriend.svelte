00326ca0eeb8753ac4e3d4c6e3286f65f270af78
Kenny <101839505+khui0@users.noreply.github.com>
2024-11-17 19:51:45 -0500

    feat: add tooltip to compact sidebar

--- src/routes/(app)/SidebarFriend.svelte
+++ src/routes/(app)/SidebarFriend.svelte
@@ -2,6 +2,8 @@
   import { timeToMs } from "$lib/time";
   import { update } from "$lib/utilities";
   import { settings } from "$lib/settings";
+  import { onMount } from "svelte";
+  import { fly } from "svelte/transition";
 
   let {
     username,
@@ -47,10 +49,23 @@
       offHours,
     };
   }
+
+  let parent: HTMLElement | undefined = $state();
+  let rect: DOMRect | undefined = $derived(parent?.getBoundingClientRect());
+  let hovering: boolean = $state(false);
+
+  onMount(() => {
+    parent?.addEventListener("pointerenter", () => {
+      hovering = true;
+    });
+    parent?.addEventListener("pointerleave", () => {
+      hovering = false;
+    });
+  });
 </script>
 
 <li>
-  <a {href} class="h-9 flex items-center">
+  <a {href} class="h-9 flex items-center" bind:this={parent}>
     <div class="w-[1.2em] flex items-center justify-center">
       <div
         class="w-3 h-3 rounded-full flex-none {status && !status.offHours
@@ -67,3 +82,18 @@
     {/if}
   </a>
 </li>
+
+{#if compact && rect && hovering}
+  <div
+    in:fly={{ x: -10, duration: 250 }}
+    out:fly={{ x: -10, duration: 250 }}
+    class="absolute flex items-center pointer-events-none left-[3.5rem] z-50"
+    style="top: {rect.top}px; height: {rect.height}px"
+  >
+    <div class="bg-base-100 rounded-badge px-2 py-1 border mx-1">
+      {$settings.obfuscateUsernames !== "true"
+        ? username
+        : username.slice(0, 1).toUpperCase() + "*".repeat(username.length - 1)}
+    </div>
+  </div>
+{/if}
