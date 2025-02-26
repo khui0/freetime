7385a103cac37581530e2837f87b0096c0aa9bb7
Kenny <101839505+khui0@users.noreply.github.com>
2025-02-26 16:34:01 -0500

    feat: add end date

--- src/routes/(app)/calendar/ExportModal.svelte
+++ src/routes/(app)/calendar/ExportModal.svelte
@@ -34,7 +34,18 @@
   function rrule(days: boolean[]) {
     const strings = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"];
     const array = strings.filter((_, i) => days[i]);
-    return `FREQ=WEEKLY;BYDAY=${array.join(",")};INTERVAL=1`;
+    return `FREQ=WEEKLY;BYDAY=${array.join(",")};INTERVAL=1;UNTIL=${lastDayOfClasses()}`;
+  }
+
+  function lastDayOfClasses() {
+    const date = new Date();
+    const endDates = ["2025-01-24", "2025-05-10", "2025-12-08"];
+    let selected = endDates.find((item) => date <= new Date(item));
+
+    if (selected) {
+      return new Date(selected).toISOString().replace(/(\.000)|[-,:,]/g, "");
+    }
+    return null;
   }
 
   function startDate(days: boolean[]) {
@@ -86,6 +97,7 @@
         </li>
       {/each}
     </ul>
+    <p>Classes End: {lastDayOfClasses()}</p>
     <button class="btn btn-sm" onclick={save}>Export (.ics)</button>
   {:else}
     <p>Unable to generate file</p>
